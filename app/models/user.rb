4bd27a132d84323718b3526045218a8eb3b3bc41
OvenAut <oliver@oszko.net>
2010-10-20 18:15:26 +0200

    Finshed microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,22 +40,21 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
   
-#  def self.authenticate(email, submitted_password)
-#    user = find_by_email(email)
-#    return nil if user.nil?
-#    return user if user.has_password?(submitted_password)
-#  end
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
-  def self.authenticate(email, submitted_password)
+  class << self
+  def authenticate(email, submitted_password)
     user = find_by_email(email) if !email.empty? && !submitted_password.empty?
     user && user.has_password?(submitted_password) ? user : nil
   end
   
-  def self.authenticate_with_salt(id, cookie_salt)
+  def authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user :nil
   end
-  
+  end
   private
   def encrypt_password
     self.salt = make_salt if new_record?
